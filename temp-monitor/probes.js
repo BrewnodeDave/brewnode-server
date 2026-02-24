8157bfa1bfe5c1dd4be416217dda76696c73603f
Brewnode Dave <dave@brewnode.co.uk>
2026-02-24 22:18:52 +0000

    fix: names

--- temp-monitor/probes.js
+++ temp-monitor/probes.js
@@ -35,11 +35,7 @@ module.exports = [
     compensate: x => round(x * 1.020492493 - 1.323124509)
   },
   {
-<<<<<<< HEAD
-    name: "Temp Glycol",//hot outlet
-=======
     name: "Temp Glycol",//cold outlet
->>>>>>> 5e1ac86a1b3d7ed44df7945d7dde40aa77a275f2
     id: '28-000007519802',
     prevValue: null,
     compensate: x => round(x * 1.023534722 - 1.509305021)
